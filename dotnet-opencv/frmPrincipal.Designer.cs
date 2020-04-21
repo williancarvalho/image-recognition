20aafe4f830097e38ca915d394d69df8242e13d8
Willian de Carvalho <willian.carvalho@dextra-sw.com>
2020-04-21 20:31:15 -0300

    Initial commit

---
+++ dotnet-opencv/frmPrincipal.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DotnetOpenCV
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBoxActions = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.btnDettectByColor = new System.Windows.Forms.Button();
+            this.txtColorToDettect = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.groupBoxActions.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBoxActions);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 593);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox);
+            this.groupBox3.Location = new System.Drawing.Point(13, 127);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(775, 454);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Image";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(769, 435);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // groupBoxActions
+            // 
+            this.groupBoxActions.Controls.Add(this.txtColorToDettect);
+            this.groupBoxActions.Controls.Add(this.btnDettectByColor);
+            this.groupBoxActions.Controls.Add(this.btnReset);
+            this.groupBoxActions.Enabled = false;
+            this.groupBoxActions.Location = new System.Drawing.Point(12, 70);
+            this.groupBoxActions.Name = "groupBoxActions";
+            this.groupBoxActions.Size = new System.Drawing.Size(776, 51);
+            this.groupBoxActions.TabIndex = 1;
+            this.groupBoxActions.TabStop = false;
+            this.groupBoxActions.Text = "Actions";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblFileName);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Image";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(6, 19);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(87, 24);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(0, 13);
+            this.lblFileName.TabIndex = 1;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(6, 19);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDettectByColor
+            // 
+            this.btnDettectByColor.Location = new System.Drawing.Point(90, 19);
+            this.btnDettectByColor.Name = "btnDettectByColor";
+            this.btnDettectByColor.Size = new System.Drawing.Size(75, 23);
+            this.btnDettectByColor.TabIndex = 3;
+            this.btnDettectByColor.Text = "Dettect";
+            this.btnDettectByColor.UseVisualStyleBackColor = true;
+            this.btnDettectByColor.Click += new System.EventHandler(this.btnDettectByColor_Click);
+            // 
+            // txtColorToDettect
+            // 
+            this.txtColorToDettect.Location = new System.Drawing.Point(171, 21);
+            this.txtColorToDettect.Name = "txtColorToDettect";
+            this.txtColorToDettect.Size = new System.Drawing.Size(22, 20);
+            this.txtColorToDettect.TabIndex = 4;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 593);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Image Processing";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.groupBoxActions.ResumeLayout(false);
+            this.groupBoxActions.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.GroupBox groupBoxActions;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtColorToDettect;
+        private System.Windows.Forms.Button btnDettectByColor;
+        private System.Windows.Forms.ColorDialog colorDialog;
+    }
+}
+
---
+++ dotnet-opencv/frmPrincipal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotnetOpenCV
+{
+    public partial class frmPrincipal : Form
+    {
+        Bitmap originalImage;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            var dialogResult = openFileDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                using (var stream = openFileDialog.OpenFile())
+                {
+                    originalImage = new Bitmap(stream, false);
+                    pictureBox.Image = originalImage;
+                    pictureBox.Refresh();
+                }
+
+                lblFileName.Text = openFileDialog.FileName;
+                groupBoxActions.Enabled = true;
+            }
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = originalImage;
+            pictureBox.Refresh();
+        }
+
+        private void btnDettectByColor_Click(object sender, EventArgs e)
+        {
+            var dialogResult = colorDialog.ShowDialog();
+
+            if(dialogResult == DialogResult.OK)
+            {
+                txtColorToDettect.BackColor = colorDialog.Color;
+            }
+        }
+    }
+}
